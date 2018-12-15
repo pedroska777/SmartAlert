a551563a024f18fab9efe26664f34dedc9234011
vishalsjsu <44108061+vishalsjsu@users.noreply.github.com>
2018-12-14 17:44:53 -0800

    Minor fixes in FCM

--- server/src/notify.py
+++ server/src/notify.py
@@ -6,8 +6,8 @@ def get_fcm_service_channel():
    return fcm.FCMNotification(api_key=FCM_SERVER_KEY)
    
 def notify_user(user, title, message):
-   token = get_app_token_pf_user(user)
-   if not token_valid():
+   token = get_app_token_of_user(user)
+   if not token_valid(token):
        Exception("User token is not valid!")
    
    fcm_service = get_fcm_service_channel()
